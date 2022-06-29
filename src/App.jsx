9463ee374b2d3355f1968d5a39d37f5614eb55f3
Ali Alaoui <greenbarca2001@gmail.com>
2022-06-29 13:41:57 +0100

    Created section of landing page to show members of DAO

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useAddress, useMetamask, useEditionDrop } from '@thirdweb-dev/react';
-import { useState, useEffect } from 'react';
+import { useAddress, useMetamask, useEditionDrop, useToken } from '@thirdweb-dev/react';
+import { useState, useEffect, useMemo } from 'react';
 
 const App = () => {
   // use the hooks from thirdweb
@@ -8,10 +8,76 @@ const App = () => {
   console.log("👋 Address:", address);
 
   const editionDrop = useEditionDrop("0xf2337Ac3B7235E0f77e30140B775Ee7781b2DBda");
+
+  const token = useToken("0xcBE080DD429EE18965Cc63b892Bc0FC5DAbbd44B");
   // state to see whether user has claimed their membership NFT
   const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
   // loading state to check whether user is currently claiming their NFT
   const [isClaiming, setIsClaiming] = useState(false);
+
+  // Holds the amount of token each member has in state.
+const [memberTokenAmounts, setMemberTokenAmounts] = useState([]);
+// The array holding all of our members addresses.
+const [memberAddresses, setMemberAddresses] = useState([]);
+
+// A fancy function to shorten someones wallet address, no need to show the whole thing.
+const shortenAddress = (str) => {
+  return str.substring(0, 6) + "..." + str.substring(str.length - 4);
+};
+
+// This useEffect grabs all the addresses of our members holding our NFT.
+useEffect(() => {
+  if (!hasClaimedNFT) {
+    return;
+  }
+
+  // Just like we did in the 7-airdrop-token.js file! Grab the users who hold our NFT
+  // with tokenId 0.
+  const getAllAddresses = async () => {
+    try {
+      const memberAddresses = await editionDrop.history.getAllClaimerAddresses(0);
+      setMemberAddresses(memberAddresses);
+      console.log("🚀 Members addresses", memberAddresses);
+    } catch (error) {
+      console.error("failed to get member list", error);
+    }
+
+  };
+  getAllAddresses();
+}, [hasClaimedNFT, editionDrop.history]);
+
+// This useEffect grabs the # of token each member holds.
+useEffect(() => {
+  if (!hasClaimedNFT) {
+    return;
+  }
+
+  const getAllBalances = async () => {
+    try {
+      const amounts = await token.history.getAllHolderBalances();
+      setMemberTokenAmounts(amounts);
+      console.log("👜 Amounts", amounts);
+    } catch (error) {
+      console.error("failed to get member balances", error);
+    }
+  };
+  getAllBalances();
+}, [hasClaimedNFT, token.history]);
+
+// Now, we combine the memberAddresses and memberTokenAmounts into a single array
+const memberList = useMemo(() => {
+  return memberAddresses.map((address) => {
+    // We're checking if we are finding the address in the memberTokenAmounts array.
+    // If we are, we'll return the amount of token the user has.
+    // Otherwise, return 0.
+    const member = memberTokenAmounts?.find(({ holder }) => holder === address);
+
+    return {
+      address,
+      tokenAmount: member?.balance.displayValue || "0",
+    }
+  });
+}, [memberAddresses, memberTokenAmounts]);
 
   useEffect(() => {
     if (!address) {
@@ -66,6 +132,29 @@ const App = () => {
       <div className="member-page">
         <h1>🍪DAO Member Page</h1>
         <p>Congratulations on being a member</p>
+        <div>
+          <div>
+            <h2>Member List</h2>
+            <table className="card">
+              <thead>
+                <tr>
+                  <th>Address</th>
+                  <th>Token Amount</th>
+                </tr>
+              </thead>
+              <tbody>
+                {memberList.map((member) => {
+                  return (
+                    <tr key={member.address}>
+                      <td>{shortenAddress(member.address)}</td>
+                      <td>{member.tokenAmount}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     );
 };
